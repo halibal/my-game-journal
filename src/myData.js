902d5326cbdcf0ead071d2fb9855a07d1985a78e
Halibal <halibal95@gmail.com>
2022-05-28 18:38:01 +0300

    pics are edited

--- src/myData.js
+++ src/myData.js
@@ -2,7 +2,7 @@ const myData = [
     {
         id: 1,
         title: 'Horizon Zero Dawn',
-        cardImage: require("./img/horizon_zero_dawn.png"),
+        cardImage: "https://steamuserimages-a.akamaihd.net/ugc/1833540893784577410/FEB298D0C90CEF58057B8D91F0ED5DDE60B1C3F4/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
         developer: 'Guerilla Games',
         developerURL: "https://www.guerrilla-games.com",
         url: 'https://store.steampowered.com/app/1151640/Horizon_Zero_Dawn_Complete_Edition/',
@@ -13,7 +13,7 @@ const myData = [
     {
         id: 2,
         title: 'It Takes Two',
-        cardImage: require("./img/it_takes_two.png"),
+        cardImage: "https://steamuserimages-a.akamaihd.net/ugc/1833540893784582698/19ADCBDC04094AA90FA058D6E62E602499CE8CD7/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
         developer: 'Hazelight Studios',
         developerURL: "https://www.hazelight.se",
         url: 'https://store.steampowered.com/app/1426210/It_Takes_Two/',
@@ -24,7 +24,7 @@ const myData = [
     {
         id: 3,
         title: 'Chivalry 2',
-        cardImage: require("./img/chivalry_2.png"),
+        cardImage: "https://steamuserimages-a.akamaihd.net/ugc/1833540893784600943/87C8F0E79B347331ABC7D821C701948EFAE7A21A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
         developer: 'Torn Banner Studios',
         developerURL: "http://tornbanner.com",
         url: 'https://store.steampowered.com/app/1824220/Chivalry_2/',
